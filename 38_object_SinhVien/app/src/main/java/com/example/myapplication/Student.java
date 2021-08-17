b03029e077ba49ccd247bf6200b6c7da2dae8c53
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-08-17 09:26:15 -0600

    first commit

--- 38_object_SinhVien/app/src/main/java/com/example/myapplication/Student.java
+++ 38_object_SinhVien/app/src/main/java/com/example/myapplication/Student.java
@@ -2,7 +2,49 @@ package com.example.myapplication;
 
 public class Student {
 
-    public String FullName;
-    public String Address;
-    public int YearOfBirth;
+    private String FullName;
+    private String Address;
+    private int YearOfBirth;
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public int getYearOfBirth() {
+        return YearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        YearOfBirth = yearOfBirth;
+    }
+
+    public Student(String fullName, String address, int yearOfBirth) {
+        FullName = fullName;
+        Address = address;
+        YearOfBirth = yearOfBirth;
+    }
+
+  /*  public void SetYearOfBirth(int ns){
+        if(ns > 1999){
+            ns = 1999;
+        }
+
+        YearOfBirth = ns;
+    }
+
+    public int GetYearOfBirth(){
+        return YearOfBirth;
+    }*/
 }
